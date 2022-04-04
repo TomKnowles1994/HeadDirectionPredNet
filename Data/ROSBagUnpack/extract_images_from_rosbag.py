7571d49f3cd2d9932c2d2c735eec3eef368b8719
TomKnowles1994 <71404211+TomKnowles1994@users.noreply.github.com>
2022-04-04 12:53:48 +0100

    Add files via upload

---
+++ Data/ROSBagUnpack/extract_images_from_rosbag.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import numpy as np
+import pandas as pd
+from scipy.spatial.transform import Rotation
+
+import os
+from glob import iglob
+
+import rosbag
+from sensor_msgs.msg import Image
+
+from cv_bridge import CvBridge
+
+preserve_pose_index = True          # True if you want to keep the original pose index as a column in the final dataset. False if you want MultiPredNet-ready data.
+preserve_pose_timestamps = True     # True if you want to keep the pose timestamps in the final dataset. False if you want MultiPredNet-ready data.
+
+bag_file = "1.bag"
+
+jpg_quality = 100
+
+compr = cv.IMWRITE_JPEG_QUALITY;
+
+quality = jpg_quality  # jpg quality is in [0,100] range, png [0,9]
+
+params = [compr, quality]
+
+bag = rosbag.Bag(bag_file, "r")
+
+bridge = CvBridge()
+
+
+# Image topic cam0
+
+timestamps = pd.read_csv("raw_images_cam0.csv", usecols = ['%time'])
+
+output_dir = "cam0/"
+
+image_topic = "/whiskeye/head/cam0/image_raw/compressed"
+
+count = 0
+
+for topic, msg, t in bag.read_messages(topics=[image_topic]):
+
+	cv_img = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="passthrough")
+
+	save_name = "{}.jpg".format(count)
+
+	save_path = os.path.join(output_dir, save_name)
+
+	cv.imwrite(save_path, cv_img, params)
+
+	count += 1
+
+bag.close()
---
+++ Data/ROSBagUnpack/unpack_rosbag.sh
@@ -0,0 +1,156 @@
+#!/usr/bin/env bash
+
+port=11321
+bagfile=$1
+help_string="Usage:\n./unpack_rosbag.sh [bagfile]"
+
+topic_list=("/whiskeye/head/pose" "/whiskeye/head/cam0/image_raw/compressed")
+file_list=("raw_pose.csv" "raw_images_cam0.csv")
+
+subprocess_ids=()
+listener_ids=()
+
+if [ -z $1 ]; then
+
+	echo "Please specify the rosbag to unpack."
+
+	echo -e $help_string
+
+	exit 1
+
+fi
+
+if [ ! -e $1 ]; then
+
+	echo "File \"$1\" not found in this directory"
+
+	echo -e $help_string
+
+	exit 1
+
+fi
+
+echo "Starting ROS instance for unpacking on port $port"
+
+export ROS_MASTER_URI=http://localhost:$port
+
+roscore -p $port >/dev/null 2>/dev/null & roscore_id=$!
+
+sleep 5
+
+echo "Starting master process here"
+echo "Do NOT close this shell until all topics are confirmed complete!"
+
+for i in ${!topic_list[@]}; do
+
+	echo "Unpacking topic ${topic_list[i]} to ${file_list[i]}"
+
+	rostopic echo -b 1.bag -p ${topic_list[i]} > ${file_list[i]} & subprocess_ids+=("$!")
+
+	echo "Subprocess dispatched on PID ${subprocess_ids[i]}"
+
+	sleep 1
+
+done
+
+function cleanup() {
+
+echo ""
+
+for i in ${!subprocess_ids[@]}; do
+
+	if kill -0 ${subprocess_ids[i]} 2>/dev/null; then
+
+		echo "Killing unfinished process ${subprocess_ids[i]}"
+
+		kill ${subprocess_ids[i]} 2>/dev/null
+
+	fi
+
+done
+
+if kill -0 $roscore_id 2>/dev/null; then
+
+        echo "Killing roscore on port $port"
+
+	kill $roscore_id
+
+fi
+
+exit 0
+
+}
+
+trap cleanup SIGINT
+
+while ! [ ${#subprocess_ids[@]} -eq 0 ]; do
+
+	for i in ${!subprocess_ids[@]}; do
+
+		if ! kill -0 ${subprocess_ids[i]} 2>/dev/null; then
+
+			echo "Process ${subprocess_ids[i]} completed"
+
+			unset subprocess_ids[i]
+
+		fi
+	
+	done
+
+	if [ ${#subprocess_ids[@]} -eq ${#listener_ids[@]} ]; then
+
+		for i in ${!listener_ids[@]}; do
+
+			echo "No more input for listener ${listener_ids[i]}; ending process"
+
+			kill -2 ${listener_ids[i]} >/dev/null 2>/dev/null
+
+			unset subprocess_ids[i] && unset listener_ids[i]
+
+		done
+	fi
+
+done
+
+echo "All files written, killing roscore on port $port"
+
+if kill -0 $roscore_id 2>/dev/null; then
+
+        kill $roscore_id 2>/dev/null
+
+fi
+
+echo "Calling image extraction script"
+
+for i in ${!topic_list[@]}; do
+	
+	if [[ ${topic_list[i]} =~ camera ]]; then
+
+		mkdir -p ${BASH_REMATCH[0]}
+
+	fi
+
+	if [[ ${topic_list[i]} =~ cam[0-9] ]]; then
+
+		mkdir -p ${BASH_REMATCH[0]}
+
+	fi
+done
+
+python3 extract_images_from_rosbag.py &
+
+wait
+
+echo "Calling postprocessing script"
+
+python3 unpack_rosbag.py &
+
+wait
+
+echo "Zipping all generated files and images to rosbag_output.zip"
+
+zip rosbag_output.zip cam0/*.jpg cam1/*.jpg *.csv *.npy >/dev/null
+
+
+
+
